b8ce1369357567bb2d86c9166e121c23b121100b
Akinsanya Joel <akinsanyajoel82@gmail.com>
2024-01-08 16:22:17 +0100

    fixed bug

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import ThemeButton from "./ThemeButton";
 
-export default function () {
+export default function Navbar() {
   return (
     <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex justify-between h-16">
