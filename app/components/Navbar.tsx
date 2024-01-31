2af2c1ffc10ff6884146303acc7bf3b8cca4411b
Akinsanya Joel <akinsanyajoel82@gmail.com>
2024-01-31 01:24:13 +0100

    change blog name

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
         <div className="flex justify-between items-center w-full">
           <Link href="/">
             <h1 className="text-2xl font-medium">
-              Simplify<span className="text-teal-500">Docs</span>
+              Simplify<span className="text-teal-500">Javascript</span>
             </h1>
           </Link>
           <ThemeButton />
